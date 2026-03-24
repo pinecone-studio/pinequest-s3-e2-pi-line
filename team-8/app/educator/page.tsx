ff603c0d32a48d636588f61adc153eccdb6b721f
25LP5135 25LP5135 <25LP5135@G302-CC0089.local>
2026-03-24 16:55:46 +0800

    WIP: save local before force-push

---
+++ team-8/app/educator/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "./_features/Dashboard";
+
+export default function EducatorPage() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <Dashboard />
+    </div>
+  );
+}
