c984b42acd70ca1dfd69ed887194d2bc64d21df8
25LP5105 25LP5105 <25LP5105@G302-CC0181.local>
2026-03-24 14:32:36 +0800

    database

---
+++ team-8/app/educator/page.tsx
@@ -0,0 +1,3 @@
+export default function EducatorPage() {
+  return <div>Educator Page</div>;
+}
---
+++ team-8/app/student/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EducatorPage() {
+  return <div>Educator Page</div>;
+}
